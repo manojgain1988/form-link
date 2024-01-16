01b5130848c9751fae5d5e4d56a035e7cfbe6432
manojgain1988 <manojgain1988@gmail.com>
2024-01-16 12:02:30 +0600

    my first commit

---
+++ form/js/index.js
@@ -0,0 +1,15 @@
+
+
+// Typing part
+
+var typed = new Typed(".typing",{
+    strings:["Web Developer.", "Front-End Develper.", "WordPress Customizer.", "Elementor Editor."],
+    typeSpeed : 80,
+    backSpeed : 80,
+});
+
+
+// Typing part end
+
+
+
---
+++ form3/js/apps.js
@@ -0,0 +1,7 @@
+
+var btn=document.getElementById("btn");
+var navmenu=document.getElementById("navmenu");
+
+btn.addEventListener('click',function(){
+    navmenu.classList.toggle("open");
+});
